6af3a31926cab2e6589f9994d262ba95e41d8ad6
ZD-ong <384474574@qq.com>
2018-04-20 16:21:02 +0800

    实现ES6和SCSS的转译以及为CSS自动化添加前缀

---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+import module1 from './module-1'
+import module2 from './module-2'
+import '../css/main.scss'
+
+module1()
+console.log('module1 success!')
+module2()
+console.log('module2 success!')
